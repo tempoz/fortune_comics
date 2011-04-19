9ac7641d1c8f5c0be46ff47fe19f21f3671c257e
Tempoz Rene <tempozrene@gmail.com>
2011-04-19 16:04:04 -0400

    Corrected xkcd script to retrieve metadata instead of whole webpage, which apparently will get you blocked if you do it enough times.

--- xkcd.sh
+++ xkcd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #XKCD
-wget -q -O ~/.fortune_comics/homepage dynamic.xkcd.com/random/comic/
-quote=`egrep -o "comics/.*?\" title=\"[^\"]*" ~/.fortune_comics/homepage | sed "s/^.*\"//g" | sed "s/&#39;/'/g" | sed "s/&quot;/\"/g" | sed "s/&lt;/</g" | sed "s/&gt;/>/g"`
-
+wget -q -O ~/.fortune_comics/homepage http://xkcd.com/info.0.json
+num=`egrep -o "\"num\": [0-9]*" ~/.fortune_comics/homepage | egrep -o "[0-9]*"`
+num=$(($RANDOM % $num))
+num=$(($num + 1))
+wget -q -O ~/.fortune_comics/homepage http://xkcd.com/$num/info.0.json
+quote=`egrep -o "\"alt\": \".*?\", \"day\"" ~/.fortune_comics/homepage | sed "s/^\"alt\": \"//g" | sed "s/\", \"day\"$//g" | sed "s/\\\\\\\\\"/\"/g" | sed "s/\\\\\\\\\\\\\\\\/\\\\\\\\/g"`
 rm ~/.fortune_comics/homepage
-
 who='XKCD'
-
 echo -e "$quote\n\t-$who"
