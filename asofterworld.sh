3219183db92dffbfec140fa6f022f8260a1a93bb
Zoey Greer <tempoz@renci.org>
2013-12-16 11:04:15 -0500

    Updated xkcd and asofterworld to match changes in the webpage formats.

--- asofterworld.sh
+++ asofterworld.sh
@@ -2,9 +2,9 @@
 
 #A Softer World
 wget -q -O ~/.fortune_comics/homepage www.asofterworld.com/
-index=`egrep -o "[0-9]*\">*fnord" ~/.fortune_comics/homepage | egrep -o "[0-9]*"`
+index=`egrep -o "[0-9]*\">fnord" ~/.fortune_comics/homepage | egrep -o "[0-9]*"`
 wget -q -O ~/.fortune_comics/homepage www.asofterworld.com/index.php?id=$index
-quote=`egrep -o "title=\".*\" onClick" ~/.fortune_comics/homepage | sed "s/title=\"//g" | sed "s/\" onClick$//g"`
+quote=`egrep -o "title=\".*\" onclick" ~/.fortune_comics/homepage | sed "s/title=\"//g" | sed "s/\" onclick$//g"`
 
 rm ~/.fortune_comics/homepage
 
--- xkcd.sh
+++ xkcd.sh
@@ -6,7 +6,7 @@ num=`egrep -o "\"num\": [0-9]*" ~/.fortune_comics/homepage | egrep -o "[0-9]*"`
 num=$(($RANDOM % $num))
 num=$(($num + 1))
 wget -q -O ~/.fortune_comics/homepage http://xkcd.com/$num/info.0.json
-quote=`egrep -o "\"alt\": \".*?\", \"day\"" ~/.fortune_comics/homepage | sed "s/^\"alt\": \"//g" | sed "s/\", \"day\"$//g" | sed "s/\\\\\\\\\"/\"/g" | sed "s/\\\\\\\\\\\\\\\\/\\\\\\\\/g"`
+quote=`egrep -o "\"alt\": \".*?\", \"img\"" ~/.fortune_comics/homepage | sed "s/^\"alt\": \"//g" | sed "s/\", \"img\"$//g" | sed "s/\\\\\\\\\"/\"/g" | sed "s/\\\\\\\\\\\\\\\\/\\\\\\\\/g"`
 rm ~/.fortune_comics/homepage
 who='XKCD'
 echo -e "$quote\n\t-$who"
